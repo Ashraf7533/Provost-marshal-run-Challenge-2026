113fc3d650cef7cdeb880d47033bcd76a4c90482
Ashraf7533 <128669515+Ashraf7533@users.noreply.github.com>
2025-12-25 11:55:54 +0500

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="localhost";
-$user="root";
-$pass="";
-$db="runchallenge2026";
-$conn=new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){ die("Database Connection Failed"); }
-?>
